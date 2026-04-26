bf3815a18f4da57f3997f29e6d5e0bbab7f70bff
AdityaputraMGL <newaditya6@gmail.com>
2026-04-27 00:41:20 +0700

    Fix: Tambah SSL buat satpam TiDB

--- src/config/database.js
+++ src/config/database.js
@@ -9,6 +9,13 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
+    // 👇 INI KUNCI SAKTINYA BANG 👇
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
+    // 👆 SAMPAI SINI 👆
     logging: false,
     pool: {
       max: 5,
